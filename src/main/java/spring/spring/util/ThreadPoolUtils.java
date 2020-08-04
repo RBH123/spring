0f24a1d2e92a8a9efa412c704e377c127e2d3b83
阮班海 <rocky.ruan@shengyecapital.com>
2020-08-04 16:30:32 +0800

    增加线程工具

--- src/main/java/spring/spring/util/ThreadPoolUtils.java
+++ src/main/java/spring/spring/util/ThreadPoolUtils.java
@@ -1,8 +1,13 @@
 package spring.spring.util;
 
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import lombok.SneakyThrows;
+import org.apache.commons.collections4.CollectionUtils;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 /**
  * All rights Reserved, Designed By www.shengyefinance.com
@@ -18,8 +23,39 @@ public class ThreadPoolUtils {
 
     private static ThreadPoolExecutor threadPoolExecutor;
 
+    private static ExecutorService executorService;
+
+    /**
+     * 拿到线程池
+     *
+     * @return
+     */
     public static ThreadPoolExecutor getInstance() {
         threadPoolExecutor = new ThreadPoolExecutor(10, 10, 0, TimeUnit.MINUTES, new LinkedBlockingDeque<>());
         return threadPoolExecutor;
+    }
+
+    @PostConstruct
+    public void init() {
+        executorService = getInstance();
+    }
+
+    @SneakyThrows
+    public <T> T execute(Callable<T> callable) {
+        Future<T> future = executorService.submit(callable);
+        return future.get();
+    }
+
+    @SneakyThrows
+    public <T> List<T> executeAll(List<Callable<T>> callableList) {
+        List<Future<T>> futures = executorService.invokeAll(callableList);
+        if (CollectionUtils.isEmpty(futures)) {
+            return Collections.EMPTY_LIST;
+        }
+        List<T> resultList = futures.stream().map(f -> {
+            T t = f.get(5, TimeUnit.SECONDS);
+            return t;
+        }).collect(Collectors.toList());
+        return resultList;
     }
 }
