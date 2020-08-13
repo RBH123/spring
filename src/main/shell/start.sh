4bc9ba589b35176eccead2de8f15c1f80a3b59d1
阮班海 <rocky.ruan@shengyecapital.com>
2020-08-13 17:47:32 +0800

    增加启动脚本

---
+++ src/main/shell/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -il
+name=rocky.docker-study
+docker ps -a | grep $name &>/dev/null
+if [ $? -eq 0 ]; then
+  echo $name" is up,we will stop and remove it!!!"
+  docker stop $name
+  docker rm $name
+else
+  echo $name" is not up!!!"
+fi
+docker images | grep $name &>/dev/null
+if [ $? -eq 0 ]; then
+  echo $name" image is existed,we will remove it!!!"
+  docker rmi $(docker images | grep $name | awk "{print $3}")
+else
+  echo $name" image is not existed!!!"
+fi
+mvn clean package -Dmaven.skip.test=true
+docker run -d -p 8083:8082 --name rocky.docker-study rocky.docker-study:latest
